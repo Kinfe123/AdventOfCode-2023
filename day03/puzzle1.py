84249c9207d4a5396c881e8c9314ce3880c41622
Kinfe123 <kinfishtech@gmail.com>
2023-12-03 15:51:16 +0300

    improved

--- day03/puzzle1.py
+++ day03/puzzle1.py
@@ -5,57 +5,32 @@ with open('./input.txt' ) as f:
         lists.append(inp[:-1])
 numbers = []
 
+
+start_indx = set()
+dxn = [[1 , 0] ,[0 , 1] , [1 , 1] , [-1, -1]]
+
 def inbound(x, y):
     return 0 <= x < len(lists) and 0 <= y < len(lists[0])
 for r in range(len(lists)):
     s = ''
     start = [-1 , -1]
     for c in range(len(lists)):
+        curr = lists[r][c]
+        if curr != '.' and not curr.isdigit():
+            for cr in range(r-1 , r+1+1):
+                for cc in range(c-1  , c+1+1):
+                    if inbound(cr, cc) and lists[cr][cc].isdigit():
+                        while cc > 0 and lists[cr][cc-1].isdigit():
+                            cc-=1
+                        start_indx.add((cr , cc))
 
-        
-        if lists[r][c].isdigit():
-            
-            
-            s+=lists[r][c]
-            if start[0] == -1 and start[1] ==-1:
-                start[0] , start[1] = r , c
-      
-        else:
-            
-            if s:
-                tr, tc = start
-                poss = False
-                for cr in [tr-1 , tr , tr+1]:
-                    for cc in [tc-1 , tc, tc+1]:
-                        if inbound(cr , cc) and lists[cr][cc] != '.' and not lists[cr][cc].isdigit():
-                            #print('found : ', lists[cr][cc]  ,(cr , cc) , start)
-                            poss = True
-                            break
-                    if poss:
-                        break
-                if poss:
-                    if s not in numbers:
-                        numbers.append(s)
-                tr , tc = r , c
-                poss = False
-                for cr in [tr-1 , tr , tr+1]:
-                    for cc in [tc-1 , tc, tc+1]:
-                        if inbound(cr , cc) and lists[cr][cc] != '.' and not lists[cr][cc].isdigit():
-                            #print('found : ', lists[cr][cc] , (cr , cc) , (r , c) )
-                            poss = True
-                            break
-                    if poss:
-                        break
-                if poss:
-                    if s not in numbers:
-                        numbers.append(s)
-                
-                
-            s = ''
-            start = [-1 , -1]
-print(numbers)
-numbers = sum([int(i) for i in numbers])
-print(numbers)
-                
-        
+
+res = 0        
+for start_x , start_y in start_indx:
+    curr_num = ''
+    while start_y < len(lists[0]) and lists[start_x][start_y].isdigit():
+        curr_num += lists[start_x][start_y]
+        start_y+=1
+    res+=int(curr_num)
+print(res)
         
