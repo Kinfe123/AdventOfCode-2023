abe135e57ac19e4b993871d88c6c3ac604ff6a02
Kinfe123 <kinfishtech@gmail.com>
2023-12-04 09:29:19 +0300

    day 04 one liner and optimize

--- day04/puzzle2.py
+++ day04/puzzle2.py
@@ -1,31 +1,35 @@
-from collections import defaultdict
-
 res = 0
-ans = defaultdict(int)
-max_tst, max_puzzle = 6 , 190
-r = [1] * max_tst
+max_tst , max_p = 6, 190
 
 with open('./test.txt') as f:
+    ans = [1]*max_tst
     for inp in f:
         inp = inp.replace('\n', '')
         semi = inp.split(':')
+        print(ans)
         cards = semi[1]
         piped = cards.split('|')
+        card_num = int(semi[0].split()[1])
+        
         
         sample  , winning = piped[0] , piped[1]
        
         sample , winning = list(map(str , sample.split())) , list(map(str , winning.split() ))
-        print(sample , winning)
+        
         set_winning = set(winning)
         score = 0
+        
         for i in sample:
             if i in set_winning:
                 score+=1
        
         if score:
-            for i in range(1 , score+1):
-                r[card_num + i] += r[i]
-                
+            for i in range(card_num+1 , card_num+score+1):
+                ans[i] += ans[card_num]
+        
+            
+            
             
         
-print(sum(r))
+        
+    print(sum(ans))
