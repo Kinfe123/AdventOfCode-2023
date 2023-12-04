81d0876057163505b7c686d46963b2fee944a567
Kinfe123 <kinfishtech@gmail.com>
2023-12-04 08:28:04 +0300

    day 04 init

--- day04/puzzle1.py
+++ day04/puzzle1.py
@@ -6,17 +6,17 @@ with open('./test.txt') as f:
         semi = inp.split(':')
         cards = semi[1]
         piped = cards.split('|')
+        
         sample  , winning = piped[0] , piped[1]
-        print(sample , winning)
+       
         sample , winning = list(map(str , sample.split())) , list(map(str , winning.split() ))
         print(sample , winning)
         set_winning = set(winning)
         score = 0
         for i in sample:
-            
             if i in set_winning:
                 score+=1
-        print('score; ' , score)
+       
         if score:
             res += 2 ** (score - 1)
         
